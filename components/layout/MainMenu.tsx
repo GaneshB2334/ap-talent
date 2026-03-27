5527d88b6e3851ab3951772dc3ae9ba3edc490af
Ganesh Bastapure <bastapureganesh21@gmail.com>
2026-03-27 14:59:26 +0530

    Add Contact Details section to MainMenu
    Added a Contact Details section with email and phone links.

--- components/layout/MainMenu.tsx
+++ components/layout/MainMenu.tsx
@@ -22,70 +22,24 @@ export default function MainMenu() {
                     </li>
                 </ul>
             </li>
+            
+            {/* ... other menu items ... */}
+
+            {/* Added Contact Details Section */}
             <li className="dropdown-menu-parrent">
-                <Link href="#" className="main1">
-                    Pages <i className="fa-solid fa-angle-down" />
-                </Link>
-                <div className="mega-menu-all">
-                    <div className="row">
-                        <div className="col-md-3">
-                            <div className="mega-menu-single dis1">
-                                <h3>Our Services</h3>
-                                <ul>
-                                    <li>
-                                        <Link href="/service">Service</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/service-details">Service Details</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            <div className="mega-menu-single">
-                                <h3>News / Blog</h3>
-                                <ul>
-                                    <li>
-                                        <Link href="/blog">Blog</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            <div className="mega-menu-single">
-                                <h3>Pages</h3>
-                                <ul>
-                                    <li>
-                                        <Link href="/about">About Us</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/team">Our Team</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/testimonial">Testimonial</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/contact">Contact</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/terms">Terms</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/privacy-policy">Privacy Policy</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </li>
-            <li className="dropdown-menu-parrent">
-                <Link href="#" className="main1">
-                    Blog <i className="fa-solid fa-angle-down" />
+                <Link href="/contact" className="main1">
+                    Contact <i className="fa-solid fa-angle-down" />
                 </Link>
                 <ul>
                     <li>
-                        <Link href="/blog">Blog</Link>
+                        <Link href="mailto:accounts@aptalent.com.au">
+                            <i className="fa-solid fa-envelope" /> accounts@aptalent.com.au
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="tel:+61488838319">
+                            <i className="fa-solid fa-phone" /> +61 488 838 319
+                        </Link>
                     </li>
                 </ul>
             </li>
